9170038e65316e65b0e9de3ae21c048ea1710edc
TolgaKoras <119622640+Xholoas@users.noreply.github.com>
2024-05-30 12:06:17 +0300

    Update stock_prediction.py

--- stock_prediction.py
+++ stock_prediction.py
@@ -1,15 +1,19 @@
 import os
+import random
+
 os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'python'
 
 from datetime import date
 import streamlit as st
 import yfinance as yf
+from yahoo_fin.stock_info import tickers_sp500
 
 import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error,mean_absolute_error
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout
@@ -17,8 +21,11 @@ from tensorflow.keras.optimizers import Adam
 
 import plotly.graph_objects as go
 
+# Get S&P 500 stock symbols
+sp500_symbols = tickers_sp500()
+
 def load_custom_css():
-    with open("custom.css","r") as file:
+    with open("custom.css", "r") as file:
         css = file.read()
         return css
 
@@ -38,6 +45,27 @@ def create_dataset(data, time_step):
         X.append(data[i:(i + time_step), 0])
         y.append(data[i + time_step, 0])
     return np.array(X), np.array(y)
+
+def calculate_metrics(actual, predicted):
+    mae = mean_absolute_error(actual, predicted)
+    mse = mean_squared_error(actual, predicted)
+    rmse = np.sqrt(mse)
+    return np.round(mae,3), np.round(mse,3), np.round(rmse,3)
+
+def suggest_symbol():
+     st.session_state.selected_stock = random.choice(sp500_symbols)
+
+def get_company_info(symbol, length=250):
+    company = yf.Ticker(symbol)
+    info = company.info
+    company_info = [
+        info.get("longName", "Not available"),
+        info.get("website", "Not available"),
+        info.get("industry", "Not available"),
+        info.get("sector", "Not available"),
+        info.get("longBusinessSummary", "Not available")[:length]
+    ]
+    return company_info
 
 @st.cache_data
 def train_lstm_model(X_train, y_train, epochs, batch_size):
@@ -60,58 +88,79 @@ def train_linear_model(X_train, y_train):
     model.fit(X_train, y_train)
     return model
 
-def forecast_lstm(model, data, scaler, time_step, forecast_period):
-    last_sequence = data[-time_step:]
-    forecast = []
-    for x in range(forecast_period):
-        current_pred = model.predict(last_sequence.reshape(1, time_step, 1))[0, 0]
-        noise = np.random.normal(loc=0, scale=0.015)
-        current_pred_with_noise = current_pred * (1 + noise)
-        forecast.append(current_pred_with_noise)
-        last_sequence = np.append(last_sequence[1:], current_pred_with_noise)
-    forecast = np.array(forecast).reshape(-1, 1)
-    forecast = scaler.inverse_transform(forecast)
-    return forecast
+def predict_historical_lstm(model, data, time_step):
+    historical_predictions = []
+    for i in range(time_step, len(data)):
+        sequence = data[i-time_step:i]
+        pred = model.predict(sequence.reshape(1, time_step, 1))[0, 0]
+        historical_predictions.append(pred)
+    historical_predictions = np.array(historical_predictions).reshape(-1, 1)
+    return historical_predictions
 
-def forecast_linear(model, data, scaler, time_step, forecast_period):
-    last_sequence = data[-time_step:]
-    forecast = []
-    for x in range(forecast_period):
-        current_pred = model.predict(last_sequence.reshape(1, -1))[0]
-        noise = np.random.normal(loc=0, scale=0.015)
-        current_pred_with_noise = current_pred * (1 + noise)
-        forecast.append(current_pred_with_noise)
-        last_sequence = np.append(last_sequence[1:], current_pred_with_noise)
-    forecast = np.array(forecast).reshape(-1, 1)
-    forecast = scaler.inverse_transform(forecast)
-    return forecast
+def predict_historical_linear(model, data, time_step):
+    X = [data[i:(i + time_step), 0] for i in range(len(data) - time_step)]
+    historical_predictions = model.predict(np.array(X))
+    historical_predictions = np.array(historical_predictions).reshape(-1, 1)
+    return historical_predictions
 
-def plot_data(data, forecast, symbol):
+def predict_future_lstm(model, last_sequence, scaler, forecast_period):
+    future_predictions = []
+    for _ in range(forecast_period):
+        pred = model.predict(last_sequence.reshape(1, len(last_sequence), 1))[0, 0]
+        noise = np.random.normal(loc=0, scale=0.025)
+        pred = pred * (1 + noise)
+        future_predictions.append(pred)
+        last_sequence = np.append(last_sequence[1:], pred)
+    future_predictions = np.array(future_predictions).reshape(-1, 1)
+    future_predictions = scaler.inverse_transform(future_predictions)
+    return future_predictions
+
+def predict_future_linear(model, last_sequence, scaler, forecast_period):
+    future_predictions = []
+    for _ in range(forecast_period):
+        pred = model.predict(last_sequence.reshape(1, -1))[0]
+        noise = np.random.normal(loc=0, scale=0.025)
+        pred = pred * (1 + noise)
+        future_predictions.append(pred)
+        last_sequence = np.append(last_sequence[1:], pred)
+    future_predictions = np.array(future_predictions).reshape(-1, 1)
+    future_predictions = scaler.inverse_transform(future_predictions)
+    return future_predictions
+
+def plot_data(data, historical_predictions, future_predictions, symbol):
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data.index, y=data['Close'], line={"color": "#508D69","width":1.5}, name="Closing Price"))
-    fig.add_trace(go.Scatter(x=pd.date_range(start=data.index[-1], periods=len(forecast)+1)[1:], y=forecast[:, 0], mode='lines', line={"color": "#f5b642","width":1.5}, name='Forecasted Price'))
+    fig.add_trace(go.Scatter(x=data.index[30:], y=data['Close'], line={"color": "#00ff37", "width": 1.5}, name="Actual Closing Price"))
+    fig.add_trace(go.Scatter(x=data.index[30:], y=historical_predictions[:, 0], line={"color": "#ffc800", "width": 1}, name='Predicted Closing Price'))
+    future_dates = pd.date_range(start=data.index[-1], periods=len(future_predictions) + 1, closed='right')
+    fig.add_trace(go.Scatter(x=future_dates, y=future_predictions[:, 0], mode='lines', line={"color": "#ff4000", "width": 1.5}, name='Future Predicted Price'))
     fig.layout.update(title_text=f"Graph of {symbol}", xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
 
 def main():
     st.title("Stock Prediction App")
-    st.markdown(f"<style>{load_custom_css()}</style>", unsafe_allow_html=True)
     start_date = "2015-01-01"
     end_date = date.today().strftime("%Y-%m-%d")
 
-    selected_stock = st.text_input("Enter a stock symbol: ", "")
-    selected_model = st.selectbox("Select a model: ", ["LSTM", "Linear"])
-    month_intervals = st.slider("Months of prediction:", 1, 4)
+    if 'selected_stock' not in st.session_state:
+        st.session_state.selected_stock = ""
+
+    selected_stock = st.text_input("Enter a stock symbol: ", st.session_state.selected_stock)
+    random_button = st.button("Get a random symbol")
+    if random_button:
+        suggest_symbol()
+    selected_model = st.selectbox("Select a model: ", [None,"LSTM", "Linear"])
+    month_intervals = st.slider("Months of prediction:", 0, 3)
     predict_button = st.button("Predict")
 
     if predict_button:
-        if selected_stock and month_intervals and selected_model:
+        if selected_stock and month_intervals>0 and selected_model != None:
             try:
-                tickers = yf.Tickers(selected_stock).tickers
-                if tickers[selected_stock] and not any(char.isdigit() for char in selected_stock):
+                if selected_stock in sp500_symbols:
                     data = load_data(selected_stock, start_date, end_date)
+                    st.write("<br>", unsafe_allow_html=True)
                     st.success(f'{selected_stock} data successfully loaded!')
-                    st.subheader(f"{selected_stock} Raw Data")
+                    st.write("<br>", unsafe_allow_html=True)
+                    st.subheader(f"{selected_stock} Last 10 Days Data")
                     st.write(data.tail(10))
 
                     scaled_data, scaler = preprocess_data(data)
@@ -127,22 +176,61 @@ def main():
                         st.error("Not enough data for training. Decrease time_step or fetch more historical data.")
                         return
 
+                    forecast_period = month_intervals * 30
                     if selected_model == "Linear":
                         model = train_linear_model(X_train, y_train)
-                        forecast = forecast_linear(model, scaled_data, scaler, time_step, month_intervals * 30)
+                        historical_predictions = predict_historical_linear(model, scaled_data, time_step)
+                        future_predictions = predict_future_linear(model, scaled_data[-time_step:], scaler, forecast_period)
                     else:
                         model = train_lstm_model(X_train, y_train, epochs=5, batch_size=64)
-                        forecast = forecast_lstm(model, scaled_data, scaler, time_step, month_intervals * 30)
+                        historical_predictions = predict_historical_lstm(model, scaled_data, time_step)
+                        future_predictions = predict_future_lstm(model, scaled_data[-time_step:], scaler, forecast_period)
 
-                    plot_data(data, forecast, selected_stock)
+                    historical_predictions = scaler.inverse_transform(historical_predictions)
+
+                    mae, mse, rmse = calculate_metrics(data['Close'].values[time_step:], historical_predictions)
+
+                    plot_data(data, historical_predictions, future_predictions, selected_stock)
+
+                    col1, col2 = st.columns(2)
+                    col3 = st.container()
+
+                    with col1:
+                        st.subheader("Used Model and Symbol")
+                        st.write(f"Symbol: {selected_stock}")
+                        st.write(f"Model: {selected_model} Model")
+                        st.write(f"Time interval: {month_intervals} Months")
+
+                    with col2:
+                        st.subheader("Model Performance Metrics")
+                        st.write(f"Mean Absolute Error (MAE): {mae}")
+                        st.write(f"Mean Squared Error (MSE): {mse}")
+                        st.write(f"Root Mean Squared Error (RMSE): {rmse}")
+
+                    with col3:
+                        st.write("<br>", unsafe_allow_html=True)
+                        st.subheader("Company Info")
+
+                        col4, col5 = st.columns(2)
+
+                        company_info = get_company_info(selected_stock)
+
+                        with col4:
+                            st.write(f"**Name:** {company_info[0]}")
+                            st.write(f"**Website:** {company_info[1]}")
+
+                        with col5:
+                            st.write(f"**Industry:** {company_info[2]}")
+                            st.write(f"**Sector:** {company_info[3]}")
+
+                        st.write(f"**Business Summary:** {company_info[4]} ...")
                 else:
                     st.error("You entered an invalid/non-existent symbol. Please try again!")
             except Exception as e:
-                st.error("An error occurred. Please try again!")
-                print(e)
+                st.error(f"An error occurred. Please try again!")
+                st.error(f"Error code: {e}")
         else:
-            st.error("Please enter a stock symbol, select a model, and choose months of prediction above.")
-
+            st.error("Please enter a stock symbol, select a model and choose months of prediction above.")
 
 if __name__ == "__main__":
     main()
